0965e35ba82526bd0f4dca83f2ca2e7fcc6ffc20
Sravan Pannala <pan.sravan@gmail.com>
2021-06-23 14:33:39 -0400

    Added stylesheet code
    Added basic stylesheet code, didn't change anything. Need to change it later

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ yr = 2020
 dff=dff[dff['Year']==yr]
 
 # App layout
-app = dash.Dash(__name__, prevent_initial_callbacks=True) # this was introduced in Dash version 1.12.0
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__,external_stylesheets=external_stylesheets,
+                prevent_initial_callbacks=True)
 app.title = 'WNBA Stats'
 server = app.server
 # Sorting operators (https://dash.plotly.com/datatable/filtering)
